382cc274690a4e210d03b64535bff16bb41ad714
GabrielleSunrise <45597740+GabrielleSunrise@users.noreply.github.com>
2021-01-04 21:29:12 +0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,14 @@
+if(window.innerWidth <= 1279) {
+document.getElementById("dop").innerHTML=document.getElementById("menu").innerHTML;
+document.getElementById("menu").innerHTML='';
+}
+
+if(window.innerWidth <= 959) {
+document.getElementById("dop3").innerHTML=document.getElementById("phone_number").innerHTML;
+document.getElementById("phone_number").innerHTML='';
+}
+
+if(window.innerWidth <= 639) {
+document.getElementById("entrance").innerHTML='';
+document.getElementById("basket").innerHTML='';
+}
---
+++ number.js
@@ -0,0 +1,3 @@
+if(window.innerWidth < 1280) {
+document.getElementById("number").innerHTML="WhatsApp";
+}
